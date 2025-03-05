56cf9f02ba448256e893bb5def0886645e097901
Bader Youssef <ibnbassem@gmail.com>
2025-03-05 11:07:53 -0500

    fixed cards and added additional resources

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,18 @@
 <template>
     <v-row class="ma-5">
         <v-col cols="12" sm="4" class="mb-4" v-for="link in links" :key="link.text">
-            <v-card class="mx-auto" :subtitle="link.subtitle" link :href="link.url" target="_blank">
+            <v-card class="mx-auto d-flex flex-column" :subtitle="link.subtitle" link :href="link.url" target="_blank"
+                height="100%">
                 <template v-slot:title>
                     <span class="font-weight-black">{{ link.title }}</span>
                 </template>
                 <v-card-text class="pt-4">
                     {{ link.text }}
                 </v-card-text>
-
+                <v-spacer></v-spacer>
                 <v-card-actions>
                     <v-btn text="See More" full append-icon="mdi-arrow-right"></v-btn>
                 </v-card-actions>
-
             </v-card>
         </v-col>
     </v-row>
@@ -44,19 +44,19 @@ const links: CardContent[] = [
     {
         title: "Polkadot Blockchain Academy",
         subtitle: "World-class, In-Person Blockchain Education",
-        text: "Two educational programs, created and delivered by key Polkadot figures. We bring blockchain and Polkadot to life for our learners, empowering next generation builders and thought leaders.",
+        text: "The Polkadot Blockchain Academy is setting the global standard in blockchain education.",
         url: "https://polkadot.academy/"
     },
     {
         title: "PBA-X",
         subtitle: "Online Blockchain Education",
-        text: "PBA-X is a flexible, part time, 4 week, cohort based, online blockchain course for anyone who wants to understand blockchain technology properly. It is designed to help you to navigate your Web3 career and is taught by the people behind Polkadot, including Dr Gavin Wood himself.",
+        text: "PBA-X is a flexible, part time, 4 week, cohort based, online blockchain course for anyone who wants to understand blockchain technology properly.",
         url: "https://pbax.polkadot.academy/"
     },
     {
         title: "DotCodeSchool",
         subtitle: "Learn to Code Web3 Apps by Building Real Projects.",
-        text: "Dot Code School is an interactive online school that teaches you how to build meaningful web3 applications using the Polkadot SDK. Learn how to build your own custom blockchain from zero to one hundred.",
+        text: "Dot Code School is an interactive online school that teaches you how to build meaningful web3 applications using the Polkadot SDK.",
         url: "https://dotcodeschool.com/"
     },
     {
@@ -64,6 +64,20 @@ const links: CardContent[] = [
         subtitle: "Your complete guide to building on Polkadot",
         text: "Your launchpad for all things Polkadot—access tools, guides, and resources to quickly start building custom chains, deploying smart contracts, and creating dApps.",
         url: "https://docs.polkadot.com/"
+    },
+
+    {
+        title: "edX Courses - Introduction to Web3 & Polkadot",
+        subtitle: "Free online courses from Web3 Foundation",
+        text: "Build the foundational knowledge required to understand the latest developments in the blockchain space and web3.",
+        url: "https://docs.polkadot.com/"
+    },
+
+    {
+        title: "Web3Educhain Tutorials",
+        subtitle: "Educating Web3 through Polkadot SDK",
+        text: "Learn how to build your own custom blockchain using the Polkadot SDK through the example and development of EduChain.",
+        url: "https://web3educhain.xyz/"
     }
 ];
 </script>
